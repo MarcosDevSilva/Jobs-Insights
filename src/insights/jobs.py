305e022b47ed47364b8cd5daa3f1c8a561208034
marco <tassio50000@gmail.com>
2024-03-26 21:20:24 -0300

    seguindo andando

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,15 +1,22 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path: str) -> None:
+        with open(path, 'r') as file:
+            reader = csv.DictReader(file)
+            self.jobs_list = list(reader)
 
     def get_unique_job_types(self) -> List[str]:
-        pass
+        return list(set(job['job_type'] for job in self.jobs_list))
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(self, crite: Dict[str, str]) -> List[dict]:
+        filtered_jobs = []
+        for job in self.jobs_list:
+            if all(job[key] == value for key, value in crite.items()):
+                filtered_jobs.append(job)
+        return filtered_jobs
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,4 +2,12 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    test_cases = [
+        ("analyze", 828),
+        ("foreign", 149),
+        ("Medical", 1639)
+    ]
+
+    for word, expected_count in test_cases:
+        count = count_ocurrences("data/jobs.csv", word)
+        assert count == expected_count
